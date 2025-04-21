e2ae8048e41cb511feee607257195f230a4474c1
Arseni Kalma <senia.kalma@gmail.com>
2025-04-21 11:32:25 +0200

    Remove not used module

--- infra/terraform-aws-modules/ecr/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ecr-url" {
-  value = aws_ecr_repository.repository.repository_url
-}
-
-output "ecr-name" {
-  value = aws_ecr_repository.repository.name
-}
--- infra/terraform-aws-modules/ecr/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "environment" {
-  default = "dev"
-}
-
-variable "name" {
-  type     = string
-  nullable = false
-}
