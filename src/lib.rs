793930eaab9378475d5d7ad66eb9b6d64038ad7c
Paul <3682187+PaulLaux@users.noreply.github.com>
2026-05-07 02:32:18 +0200

    Expand crate doc comment
    Trivial source change to exercise the docker build cache.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! Zcash Tx Tool
 //!
-//! Application based on the [Abscissa] framework.
+//! Testing utility for Zcash Shielded Assets (ZSA): builds and submits
+//! transactions against a Zebra node. Application based on the [Abscissa]
+//! framework.
 //!
 //! [Abscissa]: https://github.com/iqlusioninc/abscissa
 
